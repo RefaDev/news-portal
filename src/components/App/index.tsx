d455e6c2f10c42fa018c1cc09b9c51853e30a11b
Refat Abdukarimov <usergit@mail.ru>
2023-04-02 11:52:48 +0300

    1

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import AppContent from 'components/AppContent'
 
 const App = () => {
    return (
-      <BrowserRouter>
+      <HashRouter basename='/'>
          <AppContent />
-      </BrowserRouter>
+      </HashRouter>
    )
 }
 
